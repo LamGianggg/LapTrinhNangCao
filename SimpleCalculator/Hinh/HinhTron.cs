b854593d00ec849193f5e7b4b40a6b3c63904b4d
Lamgianggg <lgzero4350@gmail.com>
2026-09-20 22:18:31 +0700

    bai 3 - 19/9

---
+++ SimpleCalculator/Hinh/HinhTron.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace ExampleCAdvance.IHinh
+{
+    public class HinhTron : IHinh
+    {
+        private double banKinh;
+        
+        public HinhTron(double banKinh)
+        {
+            this.banKinh = banKinh;
+        }
+
+        public double getDienTich()
+        {
+            return Math.PI * banKinh * banKinh;
+        }
+
+        public double getChuVi()
+        {
+            return 2 * Math.PI * banKinh;
+        }
+    }
+}
