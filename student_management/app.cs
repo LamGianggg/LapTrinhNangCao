451dd5ae360590b17ba602c46abdfb8ed814f703
Lamgianggg <lgzero4350@gmail.com>
2026-09-20 21:51:22 +0700

    bai 3 - 19/9

---
+++ student_management/app.cs
@@ -0,0 +1,9 @@
+using ExampleCAdvance;
+using System.Runtime.ConstrainedExecution;
+using System.Linq;
+using ExampleCAdvance.Entities;
+using System.Text.RegularExpressions;
+SubjectStudent subject = new SubjectStudent();
+String Hoten = "   Nguyen Van     Thi     Anh";
+Hoten = Hoten.Trim()
+Regex.Replace(Hoten, @"\s+")
